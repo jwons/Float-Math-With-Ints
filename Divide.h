e8d790040cc24a95e53b80b50601eb393cc796fa
Kayla <kjohnson12@carthage.edu>
2017-10-13 18:35:01 -0500

    updated divide

--- Divide.h
+++ Divide.h
@@ -7,6 +7,7 @@ using namespace std;
 char convert(int num);
 void addToArray(char num, int counter, char answer[]);
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char answer[], int num);
+int digitCount(int num);
 
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char answer[], int length)
 {
@@ -37,34 +38,82 @@ bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char answer[], int l
 			c2 = c2* -1;
 		}
 
-		//calculate improper fractions to find the new numerators
-		newNumerator1 = (c1 * d1) + n1;
-		newNumerator2 = (c2 * d2) + n2;
+		//INT_MAX overload fix
+		if (c1 == INT_MAX  && n1 == 0)
+		{
+			newNumerator1 = INT_MAX;
+			newNumerator2 = (c2 * d2) + n2;
+		}
+		else if (c2 == INT_MAX && n2 == 0)
+		{
+			newNumerator1 = (c1 * d1) + n1;
+			newNumerator2 = INT_MAX;
+		}
+		else
+		{
+			//calculate improper fractions to find the new numerators
+			newNumerator1 = (c1 * d1) + n1;
+			newNumerator2 = (c2 * d2) + n2;
+		}
+
 
 		//flip second fraction multiply to get final fraction
 		finalNumerator = newNumerator1 * d2;
 		finalDenominator = d1 * newNumerator2;
 
+		//when numerator is 0 the answer will also be 0 so you can stop calculation
+		if (finalNumerator == 0)
+		{
+			addToArray('0', 0, answer);
+			addToArray('\0', 1, answer);
+			return true;
+		}
+		//can't divide by zero
+		if (finalDenominator == 0)
+		{
+			return false;
+		}
+
 		int num = 1;
 		int counter = 0;
+
+		//first # 
+		num = finalNumerator / finalDenominator;
+		
+		int digit_count = digitCount(num);
+		if (digit_count > length)
+		{
+			return false;
+		}
 
 		//if negative, add '-' to the array at position 0
 		if (isNegative == true)
 		{
-			addToArray('-', counter, answer);
+			if (num == 0)
+			{
+				addToArray('-', counter, answer);
+				++counter;
+
+				//decimal
+				addToArray('.', counter, answer);
+				++counter;
+			}
+			else
+			{
+				addToArray('-', counter, answer);
+				++counter;
+			}
+		}
+		else
+		{
+			addToArray(convert(num), counter, answer);
+			++counter;
+
+			//decimal
+			addToArray('.', counter, answer);
+
 			++counter;
 		}
-
-		//first # 
-		num = finalNumerator / finalDenominator;
-		addToArray(convert(num), counter, answer);
-
-		++counter;
-
-		//decimal
-		addToArray('.', counter, answer);
-
-		++counter;
 
 		int nextNum = num;
 		int remainder = finalNumerator;
@@ -121,6 +170,19 @@ char convert(int num)
 
 void addToArray(char num, int counter, char answer[])
 {
-	cout << "num " << num << endl;
+	//cout << "num " << num << endl;
 	answer[counter] = num;
+}
+
+int digitCount(int num)
+{
+	int count = 0;
+
+	while (num != 0)
+	{
+		num = num / 10;
+		count++;
+	}
+
+	return count;
 }
